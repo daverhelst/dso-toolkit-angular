fe12c04c54d68777513467003d4b91a89cfd589d
Dion Verhelst <dion@infoprojects.nl>
2018-10-18 13:50:57 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dso-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dso-toolkit-angular-dev';
+}
